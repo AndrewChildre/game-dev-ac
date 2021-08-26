b4dfa6dc0c3829f84dcc3e782b3f4f245c394307
Andrew Childre <35944206+AndrewChildre@users.noreply.github.com>
2021-08-26 01:05:35 +0000

    Practice basics

--- scenes/main.js
+++ scenes/main.js
@@ -1,3 +1,31 @@
-add ([
-  sprite('player')
+ const player = add ([
+  sprite('player'),
+  scale(0.8),
+  pos(20,20),
+  body()
 ])
+const moveSpeed = 200
+
+keyDown('right', () => {
+  player.move(moveSpeed, 0)
+})
+keyDown('left', () => {
+  player.move(-moveSpeed, 0)
+})
+addLevel([
+  '       ',
+  '      ',
+  '     @ ',
+  '     @  ',
+  'xxxxxxx',
+
+], {
+  width: 40,
+  height: 40,
+  'x' : [sprite('ground'), solid()],
+  '@' : [sprite('enemy'), body(), 'dangerous']
+})
+
+player.collides('dangerous', () => {
+destroy(player)
+})
